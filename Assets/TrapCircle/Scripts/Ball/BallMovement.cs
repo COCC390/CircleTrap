49bbf9b6e7bdc13e8ad1b389a3ce1ed21ab12fc6
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2023-12-13 23:30:23 +0700

    update ball movement

--- Assets/TrapCircle/Scripts/Ball/BallMovement.cs
+++ Assets/TrapCircle/Scripts/Ball/BallMovement.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     [Header("Ball init")]
-    [SerializeField] private float _initSpeed;
+    [SerializeField] private float _initTime;
+
+    private bool _canMoving = false;
 
     [Header("Ball stats")]
+    [SerializeField] private float _outerRadian = 2.3f;
+    [SerializeField] private float _innerRadian = 1.7f;
+    [SerializeField] private float _currentAngle;
     [SerializeField] private float _ballSpeed;
+
 
     void Start()
     {
-        Vector2 initTargetPos = new Vector2(this.transform.position.x, this.transform.position.y + 2);
+        Vector2 initTargetPos = new Vector2(this.transform.position.x, this.transform.position.y + _outerRadian);
 
-        StartCoroutine(InitBall(initTargetPos));
+        StartCoroutine(InitBall(initTargetPos, _initTime));
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        
+        if(_canMoving) 
+            BallMove(_outerRadian);
     }
 
-    private IEnumerator InitBall(Vector2 targetPos)
+    private void BallMove(float radius)
     {
-        Vector2.Lerp(this.transform.position, targetPos, _initSpeed * Time.deltaTime);
+        _currentAngle += _ballSpeed * Time.deltaTime;
 
-        //while (Vector2.Distance(this.transform.position, targetPos) > 0.1f)
-        //{
-        //}
-        yield return null;
+        // Calculate the new position based on the angle and radius
+        float xPos = Mathf.Cos(_currentAngle) * radius;
+        float yPos = Mathf.Sin(_currentAngle) * radius;
+
+        // Set the GameObject's position
+        transform.position = new Vector3(xPos, yPos, 0f);
+    }    
+
+    private IEnumerator InitBall(Vector2 targetPos, float time)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < time && Vector3.Distance(this.transform.position, targetPos) >= 0.1f)
+        {
+            this.transform.position = Vector2.Lerp(this.transform.position, targetPos, elapsedTime / time);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        _currentAngle = Mathf.Atan2(this.transform.position.y, this.transform.position.x);
+        _outerRadian = this.transform.position.y;
+        _canMoving = true;
+
     }
 }
